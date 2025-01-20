75fa857f4034a36be241b90be0971a4e0782fb00
Veloraios <serenityios41@gmail.com>
2025-01-20 17:47:25 +0100

    Update home.tsx

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -14,15 +14,15 @@ export function Page() {
 			<div class='mt-20 md:mt-32 relative gap-4 md:gap-6 slide-in-from-bottom-36 zoom-in-90 animate-in duration-700 fade-in w-full items-center flex flex-col after:absolute after:z-10 after:bg-[linear-gradient(to_bottom,transparent_40%,hsl(var(--bg)/0.75)_60%,hsl(var(--bg)/1)_90%)] after:inset-0  after:pointer-events-none'>
 				<div class='flex text-center items-center flex-col gap-2'>
 					<h1 class='text-5xl lg:text-6xl text-center font-secondary font-bold bg-gradient-to-br from-primary-fg to-secondary-fg bg-clip-text text-transparent z-10'>
-						Unbound
+						Serenity
 					</h1>
 					<h2 class='text-2xl lg:text-4xl text-center font-secondary font-semibold bg-gradient-to-br from-primary-fg to-secondary-fg bg-clip-text text-transparent z-10'>
-						Transform your social experience.
+						Apps/Games Without Limits
 					</h2>
 				</div>
 				<div class='flex gap-4'>
 					<Button class='min-w-40' variant='cta'>
-						Get Started
+						Install
 					</Button>
 					<Button class='min-w-40' variant='opposite' onClick={() => navigate('https://docs.unbound.rip')} asChild>
 						<a class='pointer-events-none' href='https://docs.unbound.rip'>
