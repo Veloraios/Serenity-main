493634ed7b394093391d6d6509560273772af3af
Veloraios <serenityios41@gmail.com>
2025-01-20 18:32:50 +0100

    Update faq.tsx

--- src/routes/faq.tsx
+++ src/routes/faq.tsx
@@ -13,15 +13,15 @@ export function Page() {
 				<Accordion.Content>Yes. However, Discord have never taken action against users using client modifications.</Accordion.Content>
 			</Accordion.Item>
 			<Accordion.Item value='item-2'>
-				<Accordion.Trigger>Placeholder</Accordion.Trigger>
+				<Accordion.Trigger>Is Serenity a Rat?</Accordion.Trigger>
 				<Accordion.Content>
-					Placeholder
+					No, why would we add Malware to our App? Think about it, the only way we get Money is from Selling Serenity Deluxe or Some of its Features, and it is possible with the Stripe API, so you purchase through stripe. Web APIs also only allow to fetch stuff like IP/Device Model/iOS Which Serenity doesn't do. But you can use a VPN if you don't trust it.
 				</Accordion.Content>
 			</Accordion.Item>
 			<Accordion.Item value='item-3'>
-				<Accordion.Trigger>Placeholder</Accordion.Trigger>
+				<Accordion.Trigger>What Coding Languages was Serenity made out of</Accordion.Trigger>
 				<Accordion.Content>
-					Placeholder
+					For the App we have: HTML CSS JS, NPM JS (HTML CSS JS is for Functions and UI, NPM Js is for important APIS Such as Signing/iPA Parsing) For Documentation Page: MDX, Typescript. For Download Page: Typescript, React.
 				</Accordion.Content>
 			</Accordion.Item>
 		</Accordion.Root>
