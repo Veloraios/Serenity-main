500845f0c690fec34f5df7f3126199b8258b6c1f
Veloraios <serenityios41@gmail.com>
2025-01-21 19:23:55 +0000

    test

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -2,11 +2,10 @@ import { type JSX } from 'solid-js';
 
 const Logo = (props: JSX.ImgHTMLAttributes<HTMLImageElement>) => (
   <img
-    src="https://i.imgur.com/umJ4R8l.png"
+    src="https://i.imgur.com/UPVTwlN.png"
     alt="Logo"
     width="50px"      // Set to a smaller scalable size
     height="50px"     // Set to a smaller scalable size
-    style={{ borderRadius: '15px' }}  // Making border radius fully rounded
     {...props}
   />
 );
