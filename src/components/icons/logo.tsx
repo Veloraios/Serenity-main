fb82c0833927b5c0e40aac50dce7dbbfb6db12c9
Veloraios <serenityios41@gmail.com>
2025-01-21 18:18:23 +0100

    Update logo.tsx

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -2,7 +2,7 @@ import { type JSX } from 'solid-js';
 
 const Logo = (props: JSX.ImgHTMLAttributes<HTMLImageElement>) => (
   <img
-    src="https://i.imgur.com/RUhmT7I.jpeg"
+    src="https://i.imgur.com/umJ4R8l.png"
     alt="Logo"
     width="50px"      // Set to a smaller scalable size
     height="50px"     // Set to a smaller scalable size
