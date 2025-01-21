96158d29dc3f356e4aba2e09bf6ce88126b36659
Veloraios <serenityios41@gmail.com>
2025-01-21 17:53:46 +0100

    Update logo.tsx

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -4,8 +4,9 @@ const Logo = (props: JSX.ImgHTMLAttributes<HTMLImageElement>) => (
   <img
     src="https://i.imgur.com/RUhmT7I.jpeg"
     alt="Logo"
-    width="1em"
-    height="1em"
+    width="2em"      // Adjusted to be scalable based on the font size
+    height="2em"     // Adjusted to be scalable based on the font size
+    style={{ borderRadius: '15px' }}  // Adding border radius
     {...props}
   />
 );
